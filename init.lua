773813a8ba20bafce12137fc069001fd70e2fa5b
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-04-16 23:00:15 +0200

    feat: add gitsigns movements keymaps

--- init.lua
+++ init.lua
@@ -360,6 +360,40 @@ require("lazy").setup({
 				topdelete = { text = "‾" }, ---@diagnostic disable-line: missing-fields
 				changedelete = { text = "~" }, ---@diagnostic disable-line: missing-fields
 			},
+			current_line_blame = true,
+			on_attach = function(buffer)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
+				end
+
+				map("n", "]h", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gs.nav_hunk("next")
+					end
+				end, "Next Hunk")
+				map("n", "[h", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gs.nav_hunk("prev")
+					end
+				end, "Prev Hunk")
+
+				map("n", "]H", function()
+					gs.nav_hunk("last")
+				end, "Last Hunk")
+				map("n", "[H", function()
+					gs.nav_hunk("first")
+				end, "First Hunk")
+				map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+				map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+				map({ "n", "x" }, "<leader>gd", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+				map("n", "<leader>gD", gs.reset_buffer, "Reset Buffer")
+			end,
 		},
 	},
 
