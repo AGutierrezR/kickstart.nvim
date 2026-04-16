d841d210d9a0677e424af1656fd56e9b6b2e33cb
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-04-16 18:51:32 +0200

    feat: add improve search

--- init.lua
+++ init.lua
@@ -1138,6 +1138,67 @@ require("lazy").setup({
 			},
 		},
 	},
+	{
+		"backdround/improved-search.nvim",
+		keys = {
+			{
+				"!",
+				function()
+					require("improved-search").current_word()
+				end,
+				mode = "n",
+			},
+			{
+				"g!",
+				function()
+					require("improved-search").current_word_strict()
+				end,
+				mode = "n",
+			},
+			{
+				"!",
+				function()
+					require("improved-search").in_place()
+				end,
+				mode = "x",
+			},
+			{
+				"g!",
+				function()
+					require("improved-search").in_place_strict()
+				end,
+				mode = "x",
+			},
+			{
+				"*",
+				function()
+					require("improved-search").forward()
+				end,
+				mode = "x",
+			},
+			{
+				"g*",
+				function()
+					require("improved-search").forward_strict()
+				end,
+				mode = "x",
+			},
+			{
+				"#",
+				function()
+					require("improved-search").backward()
+				end,
+				mode = "x",
+			},
+			{
+				"g#",
+				function()
+					require("improved-search").backward_strict()
+				end,
+				mode = "x",
+			},
+		},
+	},
 
 	{ -- Autocompletion
 		"saghen/blink.cmp",
